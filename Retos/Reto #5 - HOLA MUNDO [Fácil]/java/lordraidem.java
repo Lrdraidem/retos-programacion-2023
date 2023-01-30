2730dc5e6c8c676a76125582134d8c200d664c91
Lrdraidem <123333126+Lrdraidem@users.noreply.github.com>
2023-01-30 23:22:07 +0100

    Create lordraidem.java

---
+++ Retos/Reto #5 - HOLA MUNDO [Fácil]/java/lordraidem.java
@@ -0,0 +1,19 @@
+/**
+/*
+ * Escribe un !Hola Mundo! en todos los lenguajes de programación que puedas.
+ * Seguro que hay algún lenguaje que te llama la atención y nunca has utilizado,
+ * o quizás quieres dar tus primeros pasos... ¡Pues este es el momento!
+ *
+ * A ver quién se atreve con uno de esos lenguajes que no solemos ver por ahí... 
+ *
+ *
+ * @author Lordraidem
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundooo!");
+        System.out.println("Gracias Mouredev");
+        System.out.println("#PequeñaGranComunidad");
+    }
+}
